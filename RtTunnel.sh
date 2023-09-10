978284fef952aea2cc5c03a2fb12e777155e81ee
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2023-09-10 20:25:36 +0330

    update RtTunnel.sh

--- RtTunnel.sh
+++ RtTunnel.sh
@@ -23,7 +23,7 @@ configure_arguments() {
         read -p "Please Enter (IRAN IP) : " server_ip
         arguments="--kharej --iran-ip:$server_ip --iran-port:443 --toip:127.0.0.1 --toport:multiport --password:123 --sni:$sni --terminate:24"
     elif [ "$server_choice" == "1" ]; then
-        arguments="--iran --lport:443-2053 --sni:$sni --password:123 --terminate:24"
+        arguments="--iran --lport:23-65535 --sni:$sni --password:123 --terminate:24"
     else
         echo "Invalid choice. Please enter '1' or '2'."
         exit 1
