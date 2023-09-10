c33a0d6f3d0c03261b791da86e93feebf9c55a77
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2023-09-10 16:40:50 +0330

    Add files via upload

---
+++ RtTunnel.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Download and install RTT
+wget "https://raw.githubusercontent.com/radkesvat/ReverseTlsTunnel/master/install.sh" -O install.sh && chmod +x install.sh && bash install.sh
+
+# Change directory to /etc/systemd/system
+cd /etc/systemd/system
+
+# Determine server IP using curl and grep
+server_ip=$(ip -4 addr show | awk '/inet/ && !/127.0.0.1/ {gsub(/\/[0-9]+/, "", $2); print $2}')
+if [ -z "$server_ip" ]; then
+    echo "Unable to determine server IP. Please check your internet connection."
+    exit 1
+fi
+
+# Ask the user to choose a server
+echo "Which server do you want to use? (Enter 'iran' or 'foreign') : "
+read server_choice
+
+# Ask the user for SNI or default to splus.ir
+echo "Enter SNI (default is splus.ir): "
+read sni
+sni=${sni:-splus.ir}
+
+# Determine arguments based on user's choice
+if [ "$server_choice" == "foreign" ]; then
+    arguments="./RTT --kharej --iran-ip:$server_ip --iran-port:443 --toip:127.0.0.1 --toport:multiport --password:123 --sni:$sni"
+elif [ "$server_choice" == "iran" ]; then
+    arguments="./RTT --iran --lport:23-65535 --sni:$sni --password:123"
+else
+    echo "Invalid choice. Please enter 'iran' or 'foreign'."
+    exit 1
+fi
+
+# Create a new service file named tunnel.service
+cat <<EOL > tunnel.service
+[Unit]
+Description=my tunnel service
+
+[Service]
+User=root
+WorkingDirectory=/root
+ExecStart=/root/RTT $arguments
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
+# Reload systemctl daemon and start the service
+sudo systemctl daemon-reload
+sudo systemctl start tunnel.service
+sudo systemctl enable tunnel.service
