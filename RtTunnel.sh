b9d59798a1ad1307c35e00d15831c5cff6857d35
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2023-09-10 22:11:13 +0330

    update RtTunnel.sh

--- RtTunnel.sh
+++ RtTunnel.sh
@@ -30,7 +30,6 @@ configure_arguments() {
     if [ "$server_choice" == "2" ]; then
         read -p "Please Enter (IRAN IP) : " server_ip
         read -p "Please Enter Password (Please choose the same password on both servers): " password
-        password=${password:-$(< /dev/urandom tr -dc A-Za-z0-9 | head -c6)}
         arguments="--kharej --iran-ip:$server_ip --iran-port:443 --toip:127.0.0.1 --toport:multiport --password:$password --sni:$sni --terminate:24"
     elif [ "$server_choice" == "1" ]; then
         read -p "Please Enter Password (Please choose the same password on both servers): " password
